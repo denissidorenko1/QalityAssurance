20d8f68d67bda4f6fb1ea524c459b0d2af195a7a
Denis <fpv.airplane.barnaul@gmail.com>
2020-12-19 01:04:13 +0300

    static root added

--- TRSIS_lab_1/settings.py
+++ TRSIS_lab_1/settings.py
@@ -155,7 +155,7 @@ LOCALE_PATHS = [
 #SITE_ID=1
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = (os.path.join(BASE_DIR, 'mainpage/statlol'))
+STATIC_ROOT = (os.path.join(BASE_DIR, 'mainpage/static'))
 # STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'mainpage/static'),
 #    os.path.join(BASE_DIR, 'mainpage/templates/mainpage'),
